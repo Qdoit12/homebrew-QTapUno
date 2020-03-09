cd5cd71ebcdc343ffa17ea5c8b5d8a93d77ed420
Qdoit12 <56093770+Qdoit12@users.noreply.github.com>
2020-03-09 18:12:32 +0100

    Basic tap files

---
+++ Casks/veeer.rb
@@ -0,0 +1,11 @@
+cask 'veeer' do
+  version :latest
+  sha256 :no_check
+
+  # gumroad.com/d/1bf13186ecac050a955cf9d53e2e9db6 was verified as official when first introduced to the cask
+  url "https://gumroad.com/d/1bf13186ecac050a955cf9d53e2e9db6"
+  name 'VEEER'
+  homepage 'https://veeer.io/'
+
+  app 'VEEER.app'
+end
